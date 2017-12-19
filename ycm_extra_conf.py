a990f503832920aa5c89bc9a611ddd4e1b3be947
Yihan <yihan@wifi-pro-82-048.paris.inria.fr>
2017-12-19 13:35:37 +0100

    how to install YouCompleteMe

---
+++ ycm_extra_conf.py
@@ -0,0 +1,7 @@
+def FlagsForFile(filename, **kwargs):
+    flags = [
+            "--std=c++17",
+            "-I/usr/local/include",
+            "-I/usr/local/Cellar/llvm/5.0.0/include/c++/v1"
+    ]
+    return { 'flags': flags };
